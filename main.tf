720454a70b13436e4b9431d1f7cbb86fe07200f5
BItz25 <72000788+BItz25@users.noreply.github.com>
2022-09-27 16:16:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "brendacruz25/ejemplo1-pipeline"
+  name  = "learn-terraform-docker-container"
   ports {
     internal = 80
     external = 81
