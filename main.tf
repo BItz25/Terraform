277cfda78320a1562c078b3b8b99b62bf25263de
BItz25 <72000788+BItz25@users.noreply.github.com>
2022-09-27 15:53:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "repo"
+  name  = "binkuber"
   ports {
     internal = 80
     external = 81
