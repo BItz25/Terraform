a6e59cfe0d8468127a01d05a984e36114affd901
BItz25 <72000788+BItz25@users.noreply.github.com>
2022-09-27 15:35:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "lab02-terraform-erick"
+  name  = "brendacruz25"
   ports {
     internal = 80
     external = 81
