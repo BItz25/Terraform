5c5a12e5101f468be70e001f73c93d0766d63ae7
BItz25 <72000788+BItz25@users.noreply.github.com>
2022-09-27 16:14:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "brendacruz25/terra"
+  name  = "brendacruz25/ejemplo1-pipeline"
   ports {
     internal = 80
     external = 81
